5fb8ae36792a9a7f1c155856b04c83585df4be50
Arya Wibowo Jati <94813894+jateez@users.noreply.github.com>
2024-08-14 23:07:19 +0700

    feat: add routes endpoint on router.jsx

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,5 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
+import MainLayout from "../pages/MainLayout";
+import JoinRoom from "../pages/JoinRoom";
+import CreateRoom from "../pages/CreateRoom";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -8,6 +11,14 @@ const router = createBrowserRouter([
       {
         path: "",
         element: <Home />,
+      },
+      {
+        path: "/join-room",
+        element: <JoinRoom />,
+      },
+      {
+        path: "/create-room",
+        element: <CreateRoom />,
       },
     ],
   },
