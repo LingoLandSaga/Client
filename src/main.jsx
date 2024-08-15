213dbaddacdfa6e07611c0afd79bcfaf4786cc59
ariiqramadhan <ariiq.ramadhan@gmail.com>
2024-08-15 07:19:50 +0700

    feat: implement socket for the game features

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,26 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import router from "./routes/router.jsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/router.jsx';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable={false}
+            pauseOnHover
+            theme="light"
+            transition:Bounce
+        />
+        <RouterProvider router={router} />
+    </StrictMode>
 );
